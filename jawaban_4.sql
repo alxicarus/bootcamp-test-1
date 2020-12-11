0751eb39c5c93b3bbf9bff128caf1d2362b737af
alxicarus <68945406+alxicarus@users.noreply.github.com>
2020-12-11 17:34:27 +0700

    revisi jawaban_4

--- jawaban_4.sql
+++ jawaban_4.sql
@@ -1,3 +1,3 @@
-select "Country" , "Year" , max("AvgTemperature"), min("AvgTemperature") 
+select "Country" , "Year" , max("AvgTemperature") MaxTemp, min("AvgTemperature")MinTemp 
 	from bootcamp_test_alfi 
 	where ("Country" = 'Canada' or "Country" = 'Malaysia' or "Country"='Turkey') and "Year" = 2018 group by "Country", "Year" 
